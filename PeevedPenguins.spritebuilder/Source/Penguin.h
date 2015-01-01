1ffc959b73b89c0752c8cb752c6e533900fcc6ef
Victoria Simpri <victoriasimpri@Victorias-MacBook-Pro.local>
2015-01-01 14:41:51 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Victoria Simpri on 1/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
