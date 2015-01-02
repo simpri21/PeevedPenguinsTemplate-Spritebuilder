a300dcfa4f07e8ed79824fff8665fc468d9936fa
Victoria Simpri <victoriasimpri@Victorias-MacBook-Pro.local>
2015-01-02 17:57:02 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -11,7 +11,8 @@
 @implementation WaitingPenguin
 
 -(void)didLoadFromCCB {
-    float delay = (arc4random() % 2000) / 1000.f;
+    float delay = 1.f;
+    //(arc4random() % 2000) / 1000.f;
     [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
 
