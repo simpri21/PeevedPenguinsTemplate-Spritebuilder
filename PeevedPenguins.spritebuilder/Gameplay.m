0eac032aa356bfd571295a55d3a43db88a347de1
Victoria Simpri <victoriasimpri@Victorias-MacBook-Pro.local>
2015-01-02 13:57:20 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -87,6 +87,10 @@
 }
 
 -(void)sealRemoved:(CCNode *)seal {
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
+    explosion.autoRemoveOnFinish = TRUE;
+    explosion.position = seal.position
+    [seal.parent addChild:explosion];
     [seal removeFromParent];
 }
 
