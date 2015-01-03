b35ba81556144e253f3fbe4075b98681a97f3c54
Victoria Simpri <victoriasimpri@Victorias-MacBook-Pro.local>
2015-01-02 23:18:55 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -9,18 +9,18 @@
 #import "WaitingPenguin.h"
 
 @implementation WaitingPenguin
-//
-//-(void)didLoadFromCCB {
-//    float delay = (arc4random() % 2000) / 1000.f;
-//    CCLOG(@"%f", delay);
-//    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
-//}
-//
-//-(void)startBlinkAndJump {
-//    //the animation manager of each node is stored in the animation manager property
-//    CCAnimationManager *animationManager = self.animationManager;
-//    //timelines can be referenced and run by name
-//    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
-//}
+
+-(void)didLoadFromCCB {
+    float delay = (arc4random() % 2000) / 1000.f;
+    CCLOG(@"%f", delay);
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
+}
+
+-(void)startBlinkAndJump {
+    //the animation manager of each node is stored in the animation manager property
+    CCAnimationManager *animationManager = self.animationManager;
+    //timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
 
 @end
